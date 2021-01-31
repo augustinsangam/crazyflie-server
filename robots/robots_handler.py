ca171eea3b1e96f3de86a9497d42576a37f6786a
Eya-Tom Augustin SANGAM <augustin.sangam@gmail.com>
2021-01-31 12:32:39 -0500

    Update requirements.txt &7 add typing for attribute DashboardHandler.robots

--- robots/robots_handler.py
+++ robots/robots_handler.py
@@ -3,8 +3,9 @@ robot-handler.py
 Singleton class to manage robots and their states
 """
 
+from typing import Dict, Union
+
 from robots.robot import Robot
-from typing import Union
 
 
 class RobotHandler(object):
@@ -14,14 +15,25 @@ class RobotHandler(object):
     def __new__(cls):
         if RobotHandler.__instance is None:
             RobotHandler.__instance = object.__new__(cls)
-        RobotHandler.__instance.robots = dict()  # dict[str, Robot]
-        
-        # Fors tests only
-        RobotHandler.__instance.robots["robot1"] = {"name": "robot1", "speed": 1.2574998, "batteryPercentage": 96,
-                                                    "localisation": {"x": 20, "y": 2, "z": 15}, "lastUpdate": "je sais pas", "isOn": True}
-
-        RobotHandler.__instance.robots["robot2"] = {"name": "robot2", "speed": 2.8, "batteryPercentage": 58,
-                                                    "localisation": {"x": 56, "y": 21, "z": 30}, "lastUpdate": "je sais pas", "isOn": True}
+        robots: Dict[str, Robot] = {
+            "robot1": {
+                "name": "robot1",
+                "speed": 1.2574998,
+                "batteryPercentage": 96,
+                "localisation": {"x": 20, "y": 2, "z": 15},
+                "lastUpdate": "je sais pas",
+                "isOn": True
+            },
+            "robot2": {
+                "name": "robot2",
+                "speed": 2.8,
+                "batteryPercentage": 58,
+                "localisation": {"x": 56, "y": 21, "z": 30},
+                "lastUpdate": "je sais pas",
+                "isOn": True
+            }
+        }
+        RobotHandler.__instance.robots = robots
         return RobotHandler.__instance
 
     def getRobots(self) -> dict:
